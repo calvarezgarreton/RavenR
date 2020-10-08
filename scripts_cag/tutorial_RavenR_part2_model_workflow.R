50ec3aeca28a7c96e3ea97cfe5be42d360881507
Camila Desiree Alvarez Garreton <calvarezgarreton@gmail.com>
2020-10-08 14:37:57 -0300

    Create tutorial_RavenR_part2_model_workflow.R

---
+++ scripts_cag/tutorial_RavenR_part2_model_workflow.R
@@ -0,0 +1,41 @@
+rm(list=ls());
+library(RavenR)
+
+# check tz of reading functions
+# trace("forcings.read", edit=TRUE)
+# trace("custom.read",edit=TRUE)
+# trace("watershed.read",edit=TRUE)
+# this should go at the beginning of the function: tz = "GMT"
+# this should in all as.POSIXct functions: tz = tz
+
+# Load the RavenR sample data
+# ===================================================== 
+indir <- "/Applications/RavenTutorialFiles/Nith/"
+outdir <- "/Applications/RavenTutorialFiles/Nith/output/"
+fileprefix <- "Nith"
+if (dir.exists(outdir)==FALSE) {dir.create(outdir)}
+setwd(outdir)
+
+# RUN RAVEN
+# =====================================================
+# writes complete command prompt command
+# > Raven.exe [filename] -o [outputdir]
+RavenCMD <-paste(indir,"Raven_osx.exe ",indir,fileprefix," -o ",outdir,sep="") 
+system(RavenCMD) # this runs raven from the command prompt
+
+ff <- "/Applications/RavenTutorialFiles/Nith/output/run1_ForcingFunctions.csv" # replace with your own file
+ff_data <- forcings.read(ff)
+
+# pdf("forcings.pdf") # create a pdf file to direct plot to 
+dev.new()
+forcings.plot(ff_data$forcings)
+# dev.off() #finishes writing plot to .pdf file
+
+
+data(watershed.data)
+mywshd <- watershed.data$watershed.storage
+mywshd <- RavenR::watershed.read("run1_WatershedStorage.csv")$watershed.storage 
+# png("snowpack.png") # create a png file to direct plot to
+dev.new()
+plot(mywshd$snow)
+# dev.off() #finishes writing plot to .png file
